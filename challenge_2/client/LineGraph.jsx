af9ed1485d4513520584771f57750ba1521737c3
Harry Clemente <hclemente1989@gmail.com>
2021-01-08 17:06:59 -0800

    Rendered Chart, set up api call, and passed data to chart

---
+++ challenge_2/client/LineGraph.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import Chart from "chart.js";
+// import classes from "./LineGraph.module.css";
+
+export default class LineGraph extends Component {
+    constructor (props) {
+      super(props);
+      this.state = {
+
+      }
+
+    }
+    chartRef = React.createRef();
+
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+
+        new Chart(myChartRef, {
+            type: "line",
+            data: {
+                //Bring in data
+                labels: this.props.labels,
+                datasets: [
+                    {
+                        label: "Bitcoin Price",
+                        data: this.props.data,
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
+    }
+    render() {
+        return (
+            <div className={'graphContainer'}>
+                <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
+            </div>
+        )
+    }
+}
--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Chart from 'chart.js';
+import LineGraph from './LineGraph';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-
+      data: {},
+      disclaimer: '',
+      isLoading: true,
     }
   }
+  componentDidMount() {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+    .then((data) => {
+      this.setState({
+        data: data.data.bpi,
+        disclaimer: data.data.disclaimer,
+        isLoading: false,
+      });
+    })
+    .catch((err) => console.log(err));
+  }
   render() {
+    let labels = [];
+    let data = [];
+    for (var key in this.state.data) {
+      labels.push(key);
+      data.push(Math.ceil(this.state.data[key]));
+    }
+    let render;
+    if (this.state.isLoading) {
+      render = (<div>isLoading</div>)
+    } else {
+      render = <LineGraph labels={labels} data={data}/>
+    }
     return (
       <div>
-        Hello Crypto!!!
+        {render}
       </div>
     )
   }
