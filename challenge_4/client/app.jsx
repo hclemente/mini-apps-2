e9f260c48a5233a7e82010e2082aaaa2db6f3aae
Harry Clemente <hclemente1989@gmail.com>
2021-01-11 09:38:00 -0800

    Setup container and express server

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+    position: absolute;
+    left: 50%;
+    top:  50%;
+    transform: translate(-50%, -50%);
+    background: grey;
+    border: 2px solid black;
+    height: 900px;
+    width: 900px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    z-index: 0;
+`;
 
 class App extends React.Component {
   constructor (props) {
@@ -12,9 +28,9 @@ class App extends React.Component {
   render() {
 
     return (
-      <div>
-       Hello
-      </div>
+      <MainContainer>
+        Test Content
+      </MainContainer>
     )
   }
 }
